ef345f31abf1674331f5f19e2f74982b01226381
TranDucTin <106247444+TranDucTin@users.noreply.github.com>
2022-12-13 23:01:20 +0700

    Add files via upload

---
+++ header.blade.php
@@ -0,0 +1,37 @@
+<header id="header" class="module header w-full top-0 left-0 fixed z-200 down_lg:overflow-hidden bg-slate-800">
+    <div class="container">
+        <nav class="row navbar items-center bg-slate-800">
+            <div class="col  down_lg:flex down_lg:flex-wrap lg:w-1/4 header-mobile relative justify-between items-center">
+                <div class="lg:w-full py-4">
+                    <a href="#"><img class="inline" src="@asset('./images/default/logo_emcompass.jpg')"></a>
+                </div>
+                <div class="block lg:hidden">
+                    <button class="navbar-toggler hamburger-menu p-4 mt-0 cursor-pointer" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar block relative ml-auto w-16 h-2"></span>
+                        <span class="icon-bar block relative ml-auto w-16 h-2 "></span>
+                        <span class="icon-bar block relative ml-auto w-16 h-2 "></span>
+                        <span class="sr-only">Open Menu</span>
+                    </button>
+                </div>
+            </div>
+
+            <div class="col w-full lg:w-4/5 navbar-collapse main-menu flex flex-col justify-between ml-3" id="main-menu" data-module="menu">
+                <ul class="main-menu-ul navbar-nav list-none flex mb-0 p-0 text-white flex-col text-inherit lg:flex-row lg:justify-end">
+                    <li class="menu-item ">
+                        <a class="block py-9 lg:p-9 no-underline font-black" href="#">ABOUT US</a>
+                    </li>
+                    <li class="menu-item">
+                        <a class="block py-9 lg:p-9 no-underline font-black" href="#">LEADERSHIP</a>
+                    </li>
+                    <li class="menu-item ">
+                        <a class="block py-9 lg:p-9 no-underline font-black" href="#">CAREERS</a>
+                    </li>
+
+                    <li class="active menu-item">
+                        <a class="block py-9 lg:p-9 no-underline font-black" href="#">CONTACT US</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
---
+++ module-moduel_6.blade.php
@@ -0,0 +1,33 @@
+<section class="module-eight-column ">
+    <div class="container grid grid-cols-3 gap-4">
+        <div>
+            <div class="col">
+              <span class="module-eight">THE LATEST</span></div>
+            <div class="content-module-eight 3xl">
+                <img class="w-full " src="@asset('./images/default/john-schnobrich-FlPc9_VocJ4-unsplash.png')">
+                <div class="content-module-eight-1">
+                    <span class="module-eight-1 ">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy</span>
+                    <span class="module-eight-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</span>
+                    <span class="module-eight-3">READ MORE <span class="icomoon icon-chevron-down"></span></span>
+                </div>
+            </div>
+          <div class="col">
+            <div class="content-module-eight-2">
+                <div class="content-module-eight-2-1">
+                    <span class="module-eight-1">Lorem ipsum dolor sit amet, consetetur</span>
+                    <span class="module-eight-2">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</span>
+                    <span class="module-eight-3">READ MORE <span class="icomoon icon-chevron-down"></span></span>
+                </div>
+            </div>
+
+            <div class="content-module-eight-3 ">
+                <div class="content-module-eight-3-1">
+                    <span class="module-eight-1 ">Lorem ipsum dolor sit amet, consetetur</span>
+                    <span class="module-eight-2 w-full">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</span>
+                    <span class="module-eight-3 ">READ MORE <span class="icomoon icon-chevron-down"></span></span>
+                </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
