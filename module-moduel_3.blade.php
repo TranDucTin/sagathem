95a70faa6940fde8564b4a6dc2754698218d8f2b
TranDucTin <106247444+TranDucTin@users.noreply.github.com>
2022-12-15 22:58:12 +0700

    Add files via upload

---
+++ module-moduel_3.blade.php
@@ -0,0 +1,23 @@
+<section class="module-three-column">
+    <div class="container module-three-column">
+        <div class="module-page-three flex">
+            <div class="page-three-column ">
+                <div class="image-three-column-1">
+                    <img class="module-image-1 inline" src="@asset('./images/default/image-three.png')">
+                </div>
+            </div>
+            <div class="page-three-column-1">
+                <div class="content-column">
+                    <span class="Connecting-text">Connecting Your Business to What Matters Most</span>
+                </div>
+                <div class="content-column-at-bottom">
+                    <p class="Connecting-text-bottom">At Encompass, we are committed to improving the way the beverage industry works.</p>
+                    <p class="Connecting-text-bottom">How people make decisions, how they serve customers, and how they run their businesses—we bring it all together.</p>
+                    <p class="Connecting-text-bottom"> Our cloud-native, secure, and compliant solutions are optimized for continuous innovation. Our ERP solutions unlock
+                        business growth opportunities by enabling each tier of the supply chain to drive operational efficiencies, improve how
+                        people work, and meet fast-changing, unpredictable consumer demand.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
