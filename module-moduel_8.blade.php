20e80c0a7efaff47b98fad5387226512d6592a90
TranDucTin <106247444+TranDucTin@users.noreply.github.com>
2022-12-15 23:22:40 +0700

    Add files via upload

---
+++ module-moduel_8.blade.php
@@ -0,0 +1,32 @@
+<section class="module-nice-column">
+    <div class="container module-nice-column">
+        <div class="row-module-nice">
+            <span class="content-module-nice-column">Trusted By Industry Leaders Across the Supply Chain</span>
+        </div>
+        <div class="row-module-nice-2">
+            <span class="content-module-nice-column-2">We are trusted by the world’s leading manufacturers, distributors, and retailers to provide the best of what technology,
+                specifically built for the beverage and food industry, can make possible.</span>
+        </div>
+        <div class="slick-slider-demo-module-nice-column">
+            <div>
+                <img src="@asset('./images/default/Advance Beverage Company.png')">
+            </div>
+            <div >
+                <img src="@asset('./images/default/firestone_logo_small1.png')">
+            </div>
+            <div >
+                <img src="@asset('./images/default/Eagle Rock.png')">
+            </div>
+            <div >
+                <img src="@asset('./images/default/WhistlePig_Logo_Primary.png')">
+            </div>
+            <div >
+                <img src="@asset('./images/default/APB-RH Barringer.png')">
+            </div>
+            <div >
+                <img src="@asset('./images/default/store-line.png')">
+            </div>
+        </div>
+
+    </div>
+</section>
